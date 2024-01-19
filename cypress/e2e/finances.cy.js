518172b3b5076604a4fe5f92375baad1d4a7b2f6
Jana-SI <janaina.adolfo.ds@gmail.com>
2024-01-19 16:20:58 -0300

    Automatizando o cenário de cadastrar entradas

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,5 +1,14 @@
-describe('', () => {
-    it('', () => {
+
+
+describe('Transações', () => {
+    it('Cadastrar uma entrada', () => {
         cy.visit("https://devfinance-agilizei.netlify.app/#")
+
+        cy.contains("Nova Transação").click()
+        cy.get('#description').type("Dev Front")
+        cy.get('#amount').type(2500)
+        cy.get('#date').type("2024-01-02")  // yyyy-mm-dd
+
+        cy.contains("button", "Salvar").click()
     });
 });
